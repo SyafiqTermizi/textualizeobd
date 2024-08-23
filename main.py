46e10e00d3f0d96ddbf637639aad2b0955d13e87
Syafiq Termizi <73152242+SyafiqTermizi2010@users.noreply.github.com>
2024-08-23 17:51:11 +0800

    Add obd logic

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+import obd
 from textual.app import App, ComposeResult
 from textual.reactive import reactive
 from textual.widget import Widget
@@ -21,9 +24,36 @@ class OBDDisplay(App):
     CSS_PATH = "styles.tcss"
 
     def compose(self) -> ComposeResult:
-        yield Display(label="Speed (km/h)", padding=3)
-        yield Display(label="RPM (x1000)", padding=2)
-        yield Display(label="Temp (c)", padding=3)
+        yield Display(label="Speed (km/h)", padding=3, id="speed")
+        yield Display(label="RPM (x1000)", padding=2, id="rpm")
+        yield Display(label="Temp (c)", padding=3, id="temp")
+
+    def update_rpm(self, response):
+        self.query_one("#rpm").value = response.value
+
+    def update_speed(self, response):
+        self.query_one("#speed").value = response.value
+
+    def update_temp(self, response):
+        self.query_one("#temp").value = response.value
+
+    def on_mount(self):
+        self.connection = obd.Async()
+
+        self.connection.watch(obd.commands.COOLANT_TEMP, callback=self.update_temp)
+        self.connection.watch(obd.commands.RPM, callback=self.update_rpm)
+        self.connection.watch(obd.commands.SPEED, callback=self.update_speed)
+
+        self.connection.start()
+
+    def exit(
+        self,
+        result: Any | None = None,
+        return_code: int = 0,
+        message: str | None = None,
+    ) -> None:
+        self.connection.stop()
+        return super().exit(result, return_code, message)
 
 
 if __name__ == "__main__":
